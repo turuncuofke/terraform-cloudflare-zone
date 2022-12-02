6848f67450fde7737faf24b7a7a4ac5ef3445d28
turuncuofke <tanercengizz@gmail.com>
2022-12-02 16:37:06 +0300

    Add dynamic blocks to cloudflare_rate_limit resource

--- cloudflare_rate_limit.tf
+++ cloudflare_rate_limit.tf
@@ -7,8 +7,42 @@ resource "cloudflare_rate_limit" "default" {
   action {
     mode = each.value.action.mode
     timeout = lookup(each.value.action, "timeout", null)
+    dynamic "response" {
+      for_each = contains(keys(each.value.action), "response") ? [1] : []
+      content {
+        content_type = each.value.action.response.content_type
+        body = each.value.action.response.body
+      }
+    }
+  }
+  dynamic "match" {
+    for_each = contains(keys(each.value), "match") ? [1] : []
+    content {
+      dynamic "request" {
+        for_each = contains(keys(each.value.match), "request") ? [1] : []
+        content {
+          url_pattern =  lookup(each.value.match.request, "url_pattern", null)
+          schemes = lookup(each.value.match.request, "schemes", null)
+          methods = lookup(each.value.match.request, "methods", null)
+        }
+      }
+      dynamic "response" {
+        for_each = contains(keys(each.value.match), "response") ? [1] : []
+        content {
+          statuses = lookup(each.value.match.response, "statuses", null)
+          origin_traffic = lookup(each.value.match.response, "origin_traffic", null)
+          headers = each.value.match.response.headers
+        }
+      }
+    }
   }
   disabled            = lookup(each.value, "disabled", null)
   description         = lookup(each.value, "description", null)
   bypass_url_patterns = lookup(each.value, "bypass_url_patterns", null)
+  dynamic "correlate" {
+    for_each = contains(keys(each.value), "correlate") ? [1] : []
+    content {
+      by = each.value.correlate.by
+    }
+  }
 }
